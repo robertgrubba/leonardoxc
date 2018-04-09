f258750f9c17bb86d272625b1b6fb629f989b9fb
Robert Grubba <rgrubba@gmail.com>
2018-04-09 08:46:56 +0200

    fix in translation

--- language/utf8/lang-polish.php
+++ language/utf8/lang-polish.php
@@ -943,5 +943,5 @@ define("_BIG_THANKS_TO","Ogromne podziękowania dla");
 define("_AND_HIS_PROJECT","i jego projektu"); 
 define("_JS_LIBRARIES","Biblioteki JavaScript"); 
 define("_DESIGN","Projekt"); 
-define("_TICKET_SYS","Zgłoś pomysł lub problem"); 
+define("_TICKET_SYS","Zgłoś pomysł/problem"); 
 ?>
