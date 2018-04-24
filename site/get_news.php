9049bd6da35822b4f6606c1f18ff5e0957ffb455
Robert Grubba <rgrubba@gmail.com>
2018-04-24 23:44:04 +0200

    script to convert news from rss feed to config_news source

--- site/get_news.php
+++ site/get_news.php
@@ -1,23 +1,26 @@
 <?php
 
-$i = 0; // counter
-$url = "http://apache-wp-1/category/leonardoxc/feed/"; // url to parse
+$i = 0; 
+$url = "http://apache-wp-1/category/leonardoxc/feed/"; // url of news feed
 $rss = simplexml_load_file($url); // XML parser
 
+echo '<?php';
+echo '$CONF[\'news\'][\'items\']=array(';
 
 foreach($rss->channel->item as $item) {
 if ($i < 5) { // parse only 5 items
     $dto = DateTime::createFromFormat(DateTime::RSS, $item->pubDate);
-    $pubDate = $dto->format('Y-m-d');
+    $pubDate = $dto->format('Y/m/d');
     echo ($i+1).'=>array(';    	
     echo '\'active\'=>1,';
     echo '\'date\'=>\''.$pubDate.'\',';
     echo '\'link\'=>\''.$item->link.'\',';
     echo '\'text\'=>\''.$item->title.'\',';
-    echo '\'target\'=>\'_blank\',';
+    echo '\'target\'=>\'_blank\'';
     echo '),';
 }
 
 $i++;
 }
+echo ');';
 ?>
