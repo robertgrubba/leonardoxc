ce16fba316c797de755af99de220e6d1e88dd885
Robert Grubba <rgrubba@gmail.com>
2018-05-21 15:15:06 +0200

    Sezonowy Ranking Klifowy BETA

---
+++ data/ranks/202/GUI_rank_cat_2.php
@@ -0,0 +1,44 @@
+<?
+/************************************************************************/
+/* Leonardo: Gliding XC Server					                                */
+/* ============================================                         */
+/*                                                                      */
+/* Copyright (c) 2004-5 by Andreadakis Manolis                          */
+/* http://sourceforge.net/projects/leonardoserver                       */
+/*                                                                      */
+/* This program is free software. You can redistribute it and/or modify */
+/* it under the terms of the GNU General Public License as published by */
+/* the Free Software Foundation; either version 2 of the License.       */
+/************************************************************************/
+
+//-----------------------------------------------------------------------
+//-----------------------  custom league --------------------------------
+//-----------------------------------------------------------------------
+	// 	open class ,   category=2";
+	// Some config
+	$cat=1; // pg
+	$dontShowDatesSelection=1;
+	$dontShowSecondMenu=1;
+	
+	$where_clause.=" AND category=2 AND takeoffID in (17005, 17009, 17006, 12477, 12478, 17010, 17011, 17015) AND DATE<'2019-01-01' AND DATE >'2018-01-01' ";
+	require_once dirname(__FILE__)."/common_pre.php";
+
+	$sub_query = "SELECT $flightsTable.ID, userID, takeoffID , userServerID,
+  				 gliderBrandID, $flightsTable.glider as glider, cat,
+  				 MAX(FLIGHT_POINTS) as FLIGHT_POINTS, FLIGHT_KM, BEST_FLIGHT_TYPE  "
+  		. " FROM $flightsTable,$pilotsTable "
+        . " WHERE (userID!=0 AND  private=0) AND $flightsTable.userID=$pilotsTable.pilotID "
+//			AND $flightsTable.userServerID=$pilotsTable.serverID
+		." $where_clause ";
+
+
+	$query = 'select ID, userID, takeoffID, userServerID, gliderBrandID, glider, cat, FLIGHT_POINTS, FLIGHT_KM, BEST_FLIGHT_TYPE from '.$flightsTable.' where FLIGHT_KM in (select max(FLIGHT_KM) as userRecord from (SELECT '.$flightsTable.'.ID, userID, takeoffID , userServerID, gliderBrandID, '.$flightsTable.'.glider as glider, cat, FLIGHT_POINTS, FLIGHT_KM, BEST_FLIGHT_TYPE FROM '.$flightsTable.','.$pilotsTable.' WHERE (userID!=0 AND private=0) AND takeoffID in (17005, 17009, 17006, 12477, 12478, 17010, 17011, 17015) AND '.$flightsTable.'.userID='.$pilotsTable.'.pilotID  AND (cat=1) AND validated=1 AND DATE<\'2019-01-01\' AND DATE >\'2018-01-01\') z group by userID, takeoffID) order by takeoffID ';
+
+
+//var_dump($query);
+
+
+require_once dirname(__FILE__)."/common.php";
+
+
+?>
---
+++ data/ranks/202/common_pre.php
@@ -0,0 +1,46 @@
+<?
+/************************************************************************/
+/* Leonardo: Gliding XC Server					                        */
+/* ============================================                         */
+/*                                                                      */
+/* Copyright (c) 2004-5 by Andreadakis Manolis                          */
+/* http://sourceforge.net/projects/leonardoserver                       */
+/*                                                                      */
+/* This program is free software. You can redistribute it and/or modify */
+/* it under the terms of the GNU General Public License as published by */
+/* the Free Software Foundation; either version 2 of the License.       */
+/************************************************************************/
+
+/*
+*  Martin Jursa 22.05.2007: Support for NAC club filtering added
+*/
+	if (!empty($nacid)) {
+		if ($nacid!=1) $nacclubid=0;
+	}
+	$nacid=0; # force DHV
+	$nacclubid=0;
+//define url of ranking rules
+	define('RULESURL', 'http://pgxc.pl/2018/05/sezonowy-ranking-klifowy/');
+
+    // OLC km's must be > 15
+//	if ($season==2018)
+//	    $where_clause.=" AND DATE<'2019-01-01' AND DATE >'2018-01-01'";
+   
+	$where_clause.=" AND (cat=$cat) ";
+
+	// pilots must be NACid=1 (DHV) and NACmemberID>0
+	//$where_clause.=" AND $pilotsTable.NACid=$nacid AND NACmemberID=0 AND countryCode='PL' ";
+
+	// The flight mus be validated
+	$where_clause.=" AND validated=1 ";
+
+	// support for NACclub filtering
+	//if ($nacclubid) {
+	//	$where_clause.=" AND $flightsTable.NACid=$nacid AND $flightsTable.NACclubID=$nacclubid ";
+	//}
+	//$where_clause.=" GROUP BY userID,takeoffID ";
+
+
+$customRankHeader="Suma km OLC";
+
+?>
