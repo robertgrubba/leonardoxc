ae19e1d7fca2374b66706a62d8dd677eeb98fe79
Robert Grubba <rgrubba@gmail.com>
2018-10-22 14:53:03 +0200

    Update track link

--- MENU_top_menu.php
+++ MENU_top_menu.php
@@ -489,7 +489,7 @@ $arrDownImg=leoHtml::img("icon_arrow_left.gif",0,0,'','','icons1');
 ?>
 	</ul>
 </li>
-<li class="long"> <a href="http://trac.pgxc.pl/" target="_blank"><?= _TICKET_SYS ?></a></li>
+<li class="long"> <a href="http://trac.pgxc.pl/newticket" target="_blank"><?= _TICKET_SYS ?></a></li>
 </ul>
 
 
