2dfcb96dd5e6d725f1af12d7d6a0ccee1a16ebaf
Robert Grubba <rgrubba@gmail.com>
2019-05-21 00:16:01 +0200

    Updated layout: comments section and responsiveness

--- EXT_latest_comments.php
+++ EXT_latest_comments.php
@@ -2,19 +2,14 @@
 <head>
 <title>Latest leonardo comments</title>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-        <link rel="stylesheet" href="<?=$moduleRelPath?>/js/bootstrap/css/bootstrap.css" type="text/css">
-        <script src="<?=$moduleRelPath?>/js/bootstrap/css/bootstrap.css" type="text/css"></script>
-        <script src="<?=$moduleRelPath?>/js/bootstrap/js/bootstrap.min.js" type="text/javascript"/></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script defer src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script defer src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="<?=$moduleRelPath?>/templates/pgxc/style_top_menu.css" type="text/css">
 
 <base target="_parent">
 </head>
 <body>
 <?php
 	$rss = new DOMDocument();
-	$rss->load("http://apache-leonardo-tst-1/rss.php?op=comments");
+	$rss->load("http://localhost/rss.php?op=comments");
 	$feed = array();
 	foreach ($rss->getElementsByTagName('item') as $node) {
 		$item = array ( 
@@ -31,10 +26,15 @@
 		$title = str_replace(' & ', ' &amp; ', $feed[$x]['title']);
 		$link = $feed[$x]['link'];
 		$description = $feed[$x]['desc'];
-		$date = date('d.m.Y', strtotime($feed[$x]['date']));
-		echo '<p><strong><a href="'.$link.'" title="'.$title.'">'.$title.'</a></strong><br />';
-		echo '<small><em>Dnia: '.$date.' napisał: </em></small></p>';
-		echo '<p>'.$description.'</p>';
+		$date = date('d.m.Y, H:i:s', strtotime($feed[$x]['date']));
+		echo '<table width="98%" border="0" align="center" cellpadding="0" cellspacing="0" class="main_text indexTable">';
+		echo '<tr>';
+		echo '<td width="20%" valign="bottom" bgcolor="#CED8E1" class="catHeader">';
+		echo '<a style="font-size: 11px;font-family: Verdana, Arial, Helvetica, sans-serif; line-height: 110%;" href="'.$link.'" title="'.$title.'">'.$title.':</a>';
+		echo '<ul style="font-size: 10px;font-family: Verdana, Arial, Helvetica, sans-serif; line-height: 110%;font-style: italic;" >'.$description.'</ul>';
+		echo '<div align="right" style="font-size: 10px;font-family: Verdana, Arial, Helvetica, sans-serif; line-height: 110%;font-style: italic;" >('.$date.') </div>';
+		echo '<hr>';
+		echo '</td></tr></table>';
 	}
 ?>
 </body></html>
