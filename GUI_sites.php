1bd6b7e11df0c47758eb1a6328713cc07ac0f541
Robert Grubba <rgrubba@gmail.com>
2019-07-16 15:19:15 +0200

    removed garbage

--- GUI_sites.php
+++ GUI_sites.php
@@ -68,7 +68,6 @@
 		'TEMPLATE_REL_PATH'=>$moduleRelPath."/templates/".$PREFS->themeName ,
 		'LANG'=>$currentlang,
 	));
-echo "AAAAA";
    	$Ltemplate->pparse('body');
 ?>
 	<div style="text-align: right"><?
