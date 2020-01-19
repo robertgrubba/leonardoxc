dd18a7e0daf72d77e3fba0fe64db75bff2882712
Robert Grubba <rgrubba@gmail.com>
2020-01-19 01:38:51 +0100

    include newly described takeoffs on news line

--- site/get_news.php
+++ site/get_news.php
@@ -8,7 +8,26 @@ echo '<?php ';
 echo '$CONF[\'news\'][\'items\']=array(';
 
 foreach($rss->channel->item as $item) {
-if ($i < 5) { // parse only 5 items
+if ($i < 3) { // parse only 3 items
+    $dto = DateTime::createFromFormat(DateTime::RSS, $item->pubDate);
+    $pubDate = $dto->format('Y/m/d');
+    echo ($i+1).'=>array(';    	
+    echo '\'active\'=>1,';
+    echo '\'date\'=>\''.$pubDate.'\',';
+    echo '\'link\'=>\''.$item->link.'\',';
+    echo '\'text\'=>\''.$item->title.'\',';
+    echo '\'target\'=>\'_blank\'';
+    echo '),';
+}
+
+
+$i++;
+}
+$i=4;
+$url = "http://apache-leonardo-1/rss.php?op=takeoffs"; // url of news feed
+$rss = simplexml_load_file($url); // XML parser
+foreach($rss->channel->item as $item) {
+if ($i < 7) { // parse only 3 items
     $dto = DateTime::createFromFormat(DateTime::RSS, $item->pubDate);
     $pubDate = $dto->format('Y/m/d');
     echo ($i+1).'=>array(';    	
