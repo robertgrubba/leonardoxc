abc7495af242d24dddcdb3ae4bfb064018fb3de2
Robert Grubba <rgrubba@gmail.com>
2020-06-03 10:55:40 +0200

    layout change of latest posts summary

--- EXT_latest_posts.php
+++ EXT_latest_posts.php
@@ -20,8 +20,8 @@
                 echo '<tr>';
                 echo '<td width="20%" valign="bottom" bgcolor="#D7EDD3" class="catHeader">';
                 //echo ''.$date.'<em style="font-size: 11px;font-family: Verdana, Arial, Helvetica, sans-serif; line-height: 110%;font-style:regular;">:</em>';
-                echo '<ul style="font-size: 10px;font-family: Verdana, Arial, Helvetica, sans-serif; line-height: 110%;font-style: italic;" ><strong>'.$date.'</strong>: '.$content.'</ul>';
-                echo '<div align="right" style="font-size: 10px;font-family: Verdana, Arial, Helvetica, sans-serif; line-height: 110%;font-style: italic;" ><a href="https://www.facebook.com/groups/1478007302478465/" target="_blank">(liczba komentarzy: '.$replys.')</a> </div>';
+                echo '<ul style="font-size: 10px;font-family: Verdana, Arial, Helvetica, sans-serif; line-height: 110%;font-style: italic;" >'.$content.'</ul>';
+                echo '<div align="right" style="font-size: 10px;font-family: Verdana, Arial, Helvetica, sans-serif; line-height: 110%;font-style: italic;" >(ostatnia aktywność w wątku:<br>'.$date.', <br><a href="https://www.facebook.com/groups/1478007302478465/" target="_blank">liczba komentarzy: '.$replys.'</a>) </div>';
                 echo '<hr>';
                 echo '</td></tr></table>';
 
